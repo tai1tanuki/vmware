5696576438aff6dc1bd1ee54377b22918cff160a
tai1tanuki <tanuki2009.nt@gmail.com>
2016-02-17 11:23:05 +0900

    update

--- clonevm.sh
+++ clonevm.sh
@@ -2,20 +2,6 @@
 
 echo -e  "VMware virtual machine cloning tool. Ver1.0.0 (c)taiichi.nuki 2012-2016\n"
 
-# Function definition
-function info {
-  echo -e "$(date '+%Y%m%d %T') Info: $@"
-}
-
-function error {
-  echo -e "$(date '+%Y%m%d %T') Error: $@" 1>&2
-}
-
-function abort {
-  echo -e "$(date '+%Y%m%d %T') Error: $@\n" 1>&2
-  exit 1
-}
-
 # Initialization
 datastoreBase="/vmfs/volumes"
 cmdName=$(basename $0)
@@ -37,33 +23,34 @@ dstpath="$datastoreBase/$dstDatastore/$dstDirectory"
 # Parameter number check
 if [ $# -ne 3 ]; then
   echo -e "Usage: $cmdName <src-vmname> <dst-vmname> <dst-datastore>\n" 1>&2
-  abort "Parameter number is not enough."
+  echo -e "$(date '+%Y%m%d %T') Error: Parameter number is not enough." 1>&2
+  exit 1
 fi
 
 # Source virtual machine existence check
 if [ -z $vmRecord ]; then
-  abort "Source virtual machine was not found in this ESXi."
+  echo -e "$(date '+%Y%m%d %T') Error: Source virtual machine was not found in this host."
 fi
 
 # Source virtual machine power state check
 vim-cmd vmsvc/power.getstate $srcVMID | grep -q off 
 if [ $? -ne 0 ]; then
-  abort "Source virtual machine is not power off."
+  echo -e "$(date '+%Y%m%d %T') Error: Source virtual machine is not power off."
 fi
 
 # Destination virtual machine name duplicate check
 if [ ! $(cat "$vmDatabase" | grep "^$dstVMName,") ]; then
-  abort "Destination virtual machine name is duplicated."
+  echo -e "$(date '+%Y%m%d %T') Error: Destination virtual machine name is duplicated."
 fi
 
 # Destination datastore existence check
 if [ ! -d "$datastoreBase/$dstDatastore" ]; then
-  abort "Destination datastore was not found in this host."
+  echo -e "$(date '+%Y%m%d %T') Error: Destination datastore was not found in this host."
 fi
 
 # Destination virtual machine directory existence check
 if [ -d "$dstPath" ]; then
-  abort "Destination virtual machine directory is already exist."
+  echo -e "$(date '+%Y%m%d %T') Error: Destination virtual machine directory is already exist."
 fi
 
 # User confirmation 
@@ -80,37 +67,38 @@ echo -e '----------------------------------\n'
 
 read -p "Do you want to continue? (Y/[N]): " go
 if [ $(echo $go | sed -e 's/y/Y/') != Y ]; then
-  info "Operation has been canceled.\n"
+  echo -e "$(date '+%Y%m%d %T') Info: Operation has been canceled.\n"
   exit 0
 fi
 
-info "Process started."
+echo -e "$(date '+%Y%m%d %T') Info: Process started."
 
 # Source virtual machine unregistering 
-info "Source virtual machine unregistering started."
+echo -e "$(date '+%Y%m%d %T') Info: Source virtual machine unregistering started."
 vim-cmd vmsvc/unregister $srcVMID
 if [ $? -ne 0 ]; then
-  abort "Source virtual machine unregistering failed."
+  echo -e "$(date '+%Y%m%d %T') Error: Source virtual machine unregistering failed."
 fi
-info "Source virtual machine unregistering completed."
+echo -e "$(date '+%Y%m%d %T') Info: Source virtual machine unregistering completed."
 
 # Virtual Machine file copy (w/o Virtual Disk)
-info "Virtual Machine file copy started."
+echo -e "$(date '+%Y%m%d %T') Info: Virtual Machine file copy started."
 
 mkdir -p "$dstPath"
 
 for file in $(ls $srcPath | egrep -v ".*\.vmdk$|.*\.vmsn$|.*\.vswp|.*\.lck$|.*\.vmx~$"); do
   cp -p "$srcPath/$file" "$dstPath/"
   if [ $? -ne 0 ]; then
-    error "Virtual Machine file copy failed. Source=$srcPath/$file Destination=$dstPath"
-    abort "Process aborted."
+    echo -e "$(date '+%Y%m%d %T') Error: Virtual Machine file copy failed. Source=$srcPath/$file Destination=$dstPath" 1>&2
+    echo -e "$(date '+%Y%m%d %T') Error: Process aborted.\n" 1>&2
+    exit 1
   fi      
 done
 
-info "Virtual Machine file copy ended."
+echo -e "$(date '+%Y%m%d %T') Info: Virtual Machine file copy ended."
 
 # Virtual Disk cloning
-info "Virtual Disk cloning started."
+echo -e "$(date '+%Y%m%d %T') Info: Virtual Disk cloning started."
 
 for file in $(ls $srcPath/*.vmdk | egrep -v ".*-flat\.vmdk$"); do
   file=$(basename $file)
@@ -118,21 +106,24 @@ for file in $(ls $srcPath/*.vmdk | egrep -v ".*-flat\.vmdk$"); do
 
   $cmd
   if [ $? -ne 0 ]; then
-    error "Virtual Disk cloning failed. Source=$srcPath/$file Destination=$dstPath/$file CommandLine=$cmd"
-    abort "Process aborted."
+    echo -e "$(date '+%Y%m%d %T') Error: Virtual Disk cloning failed. Source=$srcPath/$file Destination=$dstPath/$file CommandLine=$cmd" 1>&2
+    echo -e "$(date '+%Y%m%d %T') Error: Process aborted.\n" 1>&2
+    exit 1
   fi      
 done
 
-info "Virtual Disk cloning ended."
+echo -e "$(date '+%Y%m%d %T') Info: Virtual Disk cloning ended."
 
 # Cloned Virtual Machine registering
-info "Cloned Virtual Machine registering started."
+echo -e "$(date '+%Y%m%d %T') Info: Cloned Virtual Machine registering started."
 vim-cmd solo/registervm "$dstPath/$dstVMName.vmx"
 if [ $? -ne 0 ]; then
-    error "Cloned Virtual Machine registering failed."
+  echo -e "$(date '+%Y%m%d %T') Error: Cloned Virtual Machine registering failed." 1>&2
+  echo -e "$(date '+%Y%m%d %T') Error: Process aborted.\n" 1>&2
+  exit 1
 fi
-info "Cloned Virtual Machine registering ended."
+echo -e "$(date '+%Y%m%d %T') Info: Cloned Virtual Machine registering ended."
 
-info "All process has been completed." 
+echo -e "$(date '+%Y%m%d %T') Info: All process has been completed." 
 
 exit 0
